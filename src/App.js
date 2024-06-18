1781ff7af6a848c43b1204c3360aed47703a775f
seongyunlee <private.seongyun@gmail.com>
2024-06-18 13:14:23 +0900

    feat: add clarity

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './css/App.css';
 import './css/common.css'
 import Router from "./router/Router";
+import {clarity} from 'react-microsoft-clarity';
+
 
 function App() {
+
+    clarity.init('mt8dtb7v4p')
+    
     return (
         <div className="container">
             <Router></Router>
